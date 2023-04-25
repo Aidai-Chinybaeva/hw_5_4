3f9653ef637f4b7d150a5b06dac0b0b935353eeb
Aidai-Chinybaeva <chinybaeva1@gmail.com>
2023-04-25 09:54:38 +0600

    Second commit

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('item/', views.ItemListCreateAPIView.as_view()),
     path('firm/', views.FirmListCreateAPIView.as_view()),
     path('category/', views.CategoryListCreateAPIView.as_view()),
+    path('item/<int:item_id>', views.ItemRetrieveUpdateDestroyAPIView.as_view()),
+    path('firm/<int:firm_id>', views.FirmRetrieveUpdateDestroyAPIView.as_view()),
+    path('category/<int:category_id>', views.CategoryRetrieveUpdateDestroyAPIView.as_view()),
 ]
